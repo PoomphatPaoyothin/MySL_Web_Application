e531583281be047dfcc8589ec68c773324abf7f7
Thivakorn <thivakorn.n@ku.th>
2022-02-24 17:03:25 +0700

    upload picture

--- backend/backend_upload/uploadfile.js
+++ backend/backend_upload/uploadfile.js
@@ -25,6 +25,27 @@ app.post('/upload/:token', (req, res) => {
         res.send(false)
     }
 })
+
+app.post('/uploadpicture/:token' , (req,res) => {
+    var token =  req.params.token
+    if(req.files){
+        const file = req.files.file
+
+        const fileName = token +'.jpg'
+        file.mv(`C:\\picture\\${fileName}` ,err=>{
+            if (err) {
+                console.log(err)
+                res.send(false)
+            } else {
+                res.send(true)
+            }
+        })
+    }
+    else {
+        res.send(false)
+    }
+})
+
 app.listen(8000, () => {
     console.log('server started')
 })
